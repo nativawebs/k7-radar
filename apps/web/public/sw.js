cf059e4ae70f5d5fdf58c40c28104818429af657
nativawebs <infonativawebs@gmail.com>
2026-07-03 14:26:28 -0500

    Stop service worker reload aborts

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -8,7 +8,5 @@ self.addEventListener("activate", (event) => {
       .keys()
       .then((cacheNames) => Promise.all(cacheNames.map((name) => caches.delete(name))))
       .then(() => self.registration.unregister())
-      .then(() => self.clients.matchAll())
-      .then((clients) => Promise.all(clients.map((client) => client.navigate(client.url))))
   );
 });
